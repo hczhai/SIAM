de28b22e6dfdb518a8ed9a8c1636ab0b2e87112f
Christian Bunker <christian@bunkerplanet.com>
2021-02-03 23:32:24 -0500

    Update plot_basis.py

--- plot_basis.py
+++ plot_basis.py
@@ -20,21 +20,36 @@ def DiatomicEnergyVsR(atom, basis, Rvals):
     -atom, string of atomic name to input to pyscf mol constructor
     -basis, string of basis name to input to pyscf mol constructor
     -Rvals, specifies range of R, can be list of vals to take, or tuple (Rmin, Rmax, # pts)
+    Returns tuple of 1d np arrs: bond lengths (Rvals) and energies (Evals)
     '''
-
-    mol = ps.gto.Mole(); # creates molecule object
-    mol.verbose = 0; # how much printout
     
-    # specify the geometry
-    atomstring = atom+' 0 0 0; '+atom + ' 0 0 '+str(Rvals[0]); #watch spacing
-    print("atomstring",atomstring);
-    mol.atom = atomstring;
+    # iter over different R vals
+    #sort by data type:
+    if( type(Rvals) == type( (1,1)) ): # make array from tuple
+        Rvals = np.linspace(Rvals[0], Rvals[1], Rvals[2]);
+        
+    # return object is np arr
+    Evals = np.zeros(len(Rvals) );
+        
+    # so Rvals is definitely a mesh of Rvals by now
+    # can run thru it and get E's
+    for i in range(len(Rvals)):
     
-    # find HF energy
-    m = ps.scf.RHF(mol);
-    print('E(HF) = %g' % m.kernel())
+        R = Rvals[i];
+        print( "R = "+str(R))
+        mol = ps.gto.Mole(); # creates molecule object
+        mol.verbose = 0; # how much printout
     
-    return; #### end diatomic energy
+        # specify the geometry
+        atomstring = atom+' 0 0 0; '+atom + ' 0 0 '+str(R); #watch spacing
+        print("atomstring",atomstring);
+        mol.atom = atomstring;
+    
+        # find HF energy
+        m= ps.scf.RHF(mol);
+        Evals[i] = m.kernel();
+    
+    return Rvals, Evals ; #### end diatomic energy
     
 #############################################################################
 #### wrappers and test funcs
@@ -46,12 +61,15 @@ def DiatomicEnergyWrapper():
     # def inputs
     atom = 'H';
     basis = 'ccpvdz';
-    Rvals = (1, 2, 3);
+    Rvals = [1,2,3,4];
     
     print("inputs = ",atom,basis,Rvals);
     
     # run func
-    DiatomicEnergyVsR(atom, basis, Rvals);
+    data = DiatomicEnergyVsR(atom, basis, Rvals);
+    
+    # make dict
+    d = dict([(basis, data)]);
     
 
 #############################################################################
