0752c944fca67935a32be997171371e932fb35c2
Christian Bunker <christian@bunkerplanet.com>
2021-06-09 16:57:41 -0400

    folder for tutorials w garnet

--- sandbox/2q_sandbox.py
+++ sandbox/2q_sandbox.py
@@ -7,3 +7,36 @@ play around with the second quantized functions
   cre_a: create up electron
   cre_b: create down electron
 '''
+
+import numpy as np
+from pyscf import fci
+
+verbose = True;
+
+#### play with det strings
+det = [0,3,9];
+nelec = 1;
+alldetstrings = fci.cistring.make_strings(det, nelec); # all binary string occ possibilites
+                                                # encoded as (decimal) int
+    
+state = fci.cistring._gen_occslst(det, nelec);
+if(verbose):
+    print("input occupied orbital list = \n",state);
+
+#### create 1e state from vaccuum
+
+# input controls
+norb = 3; # number molecular orbitals
+nanb = 1,1; # number up, down electrons
+orbi = 1; # which orbital to fill
+
+# create e
+for i in range(1,3):
+    state = fci.addons.cre_a(state, norb, nanb, i ); # returns slater det
+
+    if(verbose):
+        print(i, "output occupied orbital list = \n", state);
+
+
+
+
