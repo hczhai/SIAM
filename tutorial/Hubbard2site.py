65288a513007aba671a46c9e406c04282a6340dd
Christian Bunker <christian@bunkerplanet.com>
2021-06-10 17:55:37 -0400

    Update Hubbard2site.py

--- tutorial/Hubbard2site.py
+++ tutorial/Hubbard2site.py
@@ -22,25 +22,23 @@ norbs = 2;
 nelecs = 2;
 epsilon = 0 # on site energy
 t = 3.0 # hopping
-U = 2000.0; # hubbard repulsion strength
+U = 200.0; # hubbard repulsion strength
 
 
 # implement h1e and h2e
 # doing it this way forces floats which is a good fail safe
 h1e = np.zeros((norbs,norbs));
-h2e = np.zeros((norbs,norbs,norbs,norbs));
+h2e = np.full((norbs,norbs,norbs,norbs),U);
 
 # put in on site energy
-#h1e[0,0], h1e[1,1] = epsilon, epsilon;
+h1e[0,0], h1e[1,1] = epsilon, epsilon;
 
 # hopping
-#h1e[0,1], h1e[1,0] = t, t;
+h1e[0,1], h1e[1,0] = t, t;
 
 # hubbard
-#h2e[0,0,0,0], h2e[1,1,1,1] = U, U;
-h2e[0,0,0,0] = U
-
-print(h1e);
+h2e[0,0,0,0] += U;
+h2e[1,1,1,1] += U;
 
 # implement FCISolver object
 cisolver = fci.direct_spin1.FCI();
@@ -59,10 +57,11 @@ if(verbose):
     
 # repeat w spin pol
 # na, nb = 1,1 gives same as above but 2,0 gives E = 0
+newnelecs = (2,1)
 cisolver = fci.direct_spin1.FCI();
-E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, (2,0));
+E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, newnelecs,nroots=4);
 if(verbose):
-    print("\n2. nelecs = 2,0");
+    print("\n2. nelecs = ",newnelecs);
     print("FCI energies = ", E_fci);
     print(v_fci);
 
@@ -78,8 +77,8 @@ if(verbose):
 cisolver0 = fci.direct_spin0.FCI()
 e_fci0, v_fci0 = cisolver.kernel(h1e, h2e, norbs, nelecs, nroots =4);
 if(verbose):
-    print("\n4. nroots = 2");
-    print("FCI energies = ", e_fci0);
+    print("\n4. extra roots");
+    print("FCI energies = ", e_fci0-epsilon);
     print(v_fci0);
     
 
