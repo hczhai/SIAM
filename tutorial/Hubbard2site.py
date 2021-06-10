db1cf4b4a26e3680ffb73963d1bcbfafb219bee7
Christian Bunker <christian@bunkerplanet.com>
2021-06-09 22:25:47 -0400

    Hubbard model implemented, not fully understood

--- tutorial/Hubbard2site.py
+++ tutorial/Hubbard2site.py
@@ -4,42 +4,71 @@
 #
 
 '''
+Template:
 Solve FCI problem with given 1-electron and 2-electron Hamiltonian
+
+Secific problem:
+2 site hubbard model
 '''
 
-import numpy
+import numpy as np
 from pyscf import fci
 
-numpy.random.seed(12)
-norb = 6
-h1 = numpy.random.random((norb,norb))
-h2 = numpy.random.random((norb,norb,norb,norb))
-# Restore permutation symmetry
-h1 = h1 + h1.T
-h2 = h2 + h2.transpose(1,0,2,3)
-h2 = h2 + h2.transpose(0,1,3,2)
-h2 = h2 + h2.transpose(2,3,0,1)
+verbose = True;
 
-#
-# Generally, direct_spin1.kernel can handle all systems.
-#
-e, fcivec = fci.direct_spin1.kernel(h1, h2, norb, 8, verbose=5)
+# system inputs
+norbs = 2;
+nelecs = 2;
+epsilon = 0 # on site energy
+t = 3 # hopping
+U = 2000; # hubbard repulsion strength
 
-#
-# A better way is to create a FCI (=FCISolver) object because FCI object offers
-# more options to control the calculation.
-#
-cisolver = fci.direct_spin1.FCI()
-cisolver.max_cycle = 100
-cisolver.conv_tol = 1e-8
-e, fcivec = cisolver.kernel(h1, h2, norb, 8)
-e, fcivec = cisolver.kernel(h1, h2, norb, (5,4))  # 5 alpha, 4 beta electrons
-e, fcivec = cisolver.kernel(h1, h2, norb, (3,1))  # 3 alpha, 1 beta electrons
 
-#
-# If you are sure the system ground state is singlet, you can use spin0 solver.
-# Spin symmetry is considered in spin0 solver to reduce cimputation cost.
-#
-cisolver = fci.direct_spin0.FCI()
-cisolver.verbose = 5
-e, fcivec = cisolver.kernel(h1, h2, norb, 8)
+# set up 1e hamiltonian (on site and hopping)
+# ie h_pq matrix elems
+h1 = np.array([[epsilon, t],[t, epsilon]]);
+
+# set up 2e hamiltonian (hubbard)
+# ie v_pqrs matrix elems
+h2 = np.zeros((norbs, norbs, norbs, norbs));
+h2[0,0,0,0] = U;
+h2[1,1,1,1] = U;
+
+# implement FCISolver object
+cisolver = fci.direct_spin1.FCI();
+cisolver.max_cycle = 100; # max number of iterations
+cisolver.conv_tol = 1e-8; # energy convergence
+
+# kernel takes (1e ham, 2e ham, num orbitals, num electrons
+# returns eigvals and eigvecs of ham (in what basis?)
+E_fci, v_fci = cisolver.kernel(h1, h2, norbs, nelecs); #since spin is none, nelecs=1,1
+if(verbose):
+    print("\n1. nelecs = 1,1");
+    print("FCI energies = ", E_fci);
+    print(v_fci);
+    
+# repeat w spin pol
+# na, nb = 1,1 gives same as above but 2,0 gives E = 0
+cisolver = fci.direct_spin1.FCI();
+E_fci, v_fci = cisolver.kernel(h1, h2, norbs, (2,0));
+if(verbose):
+    print("\n2. nelecs = 2,0");
+    print("FCI energies = ", E_fci);
+    print(v_fci);
+
+# should work same as first example
+cisolver0 = fci.direct_spin0.FCI()
+e_fci0, v_fci0 = cisolver.kernel(h1, h2, norbs, nelecs);
+if(verbose):
+    print("\n3. spin0 solver");
+    print("FCI energies = ", e_fci0);
+    print(v_fci0);
+    
+# for some reason when nroots =2 both energies output to 0, why?
+cisolver0 = fci.direct_spin0.FCI()
+e_fci0, v_fci0 = cisolver.kernel(h1, h2, norbs, nelecs, nroots =2);
+if(verbose):
+    print("\n4. nroots = 2");
+    print("FCI energies = ", e_fci0);
+    print(v_fci0);
+
