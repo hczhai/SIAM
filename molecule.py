1bdbb42816bfcdb03cd3197c8736e6bfc0942d8c
Christian Bunker <christian@bunkerplanet.com>
2021-06-11 12:59:08 -0400

    Update molecule.py

--- molecule.py
+++ molecule.py
@@ -23,7 +23,7 @@ norbs = 3;
 alpha = 0.0;
 D = 20;
 E = 10;
-U = 100;
+U = 1000;
 
 # implement h1e, h2e
 h1e = np.zeros((norbs, norbs));
@@ -37,7 +37,7 @@ h1e[2,1] = 2*alpha;
 h1e[0,0] = -D;
 h1e[2,2] = -D;
 h1e[0,2] = 2*E;
-h1e[2,0] = 2*alpha;
+h1e[2,0] = 2*E;
 
 # double electron terms
 h2e[0,0,0,0] = U;
@@ -48,9 +48,29 @@ h2e[2,2,2,2] = U;
 cisolver = fci.direct_spin1.FCI()
 cisolver.max_cycle = 100
 cisolver.conv_tol = 1e-8
-E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, nelecs,nroots=8);
+E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, nelecs,nroots=15);
+E_fci.sort();
 if(verbose):
+    np.set_printoptions(suppress=True); # no sci notatation printing
     print("\n1. nelecs = ",nelecs); # this matches analytical gd state
-    print("FCI energies = ", E_fci-(U-2*D));
-    print(v_fci);
+    print("FCI energies = ",E_fci - (U-2*D));
+    
+    
+    
+#### get correct energies
+
+# from diag'd T sector
+E_exact = np.array([0,U,0.5*U - 0.5*np.sqrt(U*U+64*E*E),0.5*U + 0.5*np.sqrt(U*U+64*E*E)]);
+
+# from diag'd other sector
+E_exact = np.append(E_exact,[-D+2*E,-D+2*E,-D-2*E,-D-2*E,-2*D+U]);
+
+# sort and print
+E_exact.sort();
+if(verbose):
+    print("\n2. Analytical solution")
+    print("Exact energies = ",E_exact);
+    print("Expected singlet energy, E/U-->0 = ",-16*E*E/U)
+
+
 
