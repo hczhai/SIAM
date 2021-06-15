b5ff6fe741d0d53c9e35b0dbacb38c97388692c2
Christian Bunker <christian@bunkerplanet.com>
2021-06-14 22:08:10 -0400

    Update Hubbard2site.py

--- tutorial/Hubbard2site.py
+++ tutorial/Hubbard2site.py
@@ -7,9 +7,16 @@
 Template:
 Solve FCI problem with given 1-electron and 2-electron Hamiltonian
 
-Secific problem:
+Specific problem:
 2 site hubbard model
 
+Formalism:
+- h1e = (p|h|q) p,q spatial orbitals
+- h2e = (pq|h|rs) chemists notation, <pr|h|qs> physicists notation
+- all direct_x solvers already assume 4 fold symmetry from sum_{pqrs}
+- hermicity means h_pqrs = h_qpsr
+- as result of identity E_pq,rs = E_rs,pq, h_pqrs = h_rspq
+- thus there is additional symmetry
 '''
 
 import numpy as np
@@ -20,102 +27,98 @@ np.set_printoptions(suppress=True); # no sci notatation printing
 
 # system inputs
 # hamiltonian params must be floats
-nelecs = (2,0); # put in all spins as spin up
-norbs = 4;      # now this means spin orbitals
-nroots = 6;
-epsilon = 0 # on site energy
+epsilon = 0.0 # on site energy
 t = 3.0 # hopping
 U = 200.0; # hubbard repulsion strength
 if(verbose):
     print("\nInputs:\nepsilon = ",epsilon,"\nt = ",t,"\nU = ",U);
     
 # analytical solution
-E_exact = np.array([0,U,U/2 - 1/2 *np.sqrt(16*t*t+U*U), U/2 + 1/2 *np.sqrt(16*t*t+U*U),0,0])
+E_exact = np.array([0,U,U*0.5 - 0.5*np.sqrt(16*t*t+U*U), U/2 + 1/2 *np.sqrt(16*t*t+U*U),0,0])
 E_exact.sort();
 if(verbose):
     print("\n0. Analytical solution");
     print("Exact energies = ", E_exact);
+    
 
+#### conventional (spin free) method
+norbs = 2;
+nelecs = 1,1;
+nroots = 6;
 
 # implement h1e and h2e
 # doing it this way forces floats which is a good fail safe
 h1e = np.zeros((norbs,norbs));
 h2e = np.zeros((norbs,norbs,norbs,norbs));
 
+# put in on site energy
+h1e[0,0] = epsilon;
+h1e[1,1] = epsilon;
+
 # hopping
-h1e[0,2] = t;
-h1e[2,0] = t;
-h1e[1,3] = t;
-h1e[3,1] = t;
+h1e[0,1] = t;
+h1e[1,0] = t;
 
 # hubbard
-if True:
-    h2e[0,0,1,1] = 2*U;
-    h2e[2,2,3,3] = 2*U;
-else:
-    h2e[0,1,1,0] = -U;
-    h2e[1,0,0,1] = -U;
-    h2e[2,3,3,2] = -U;
-    h2e[3,2,2,3] = -U;
+h2e[0,0,0,0] += U;
+h2e[1,1,1,1] += U;
 
 # implement FCISolver object
-cisolver = fci.direct_nosym.FCI();
+cisolver = fci.direct_spin1.FCI();
 cisolver.max_cycle = 100; # max number of iterations
 cisolver.conv_tol = 1e-8; # energy convergence
 
 # kernel takes (1e ham, 2e ham, num orbitals, num electrons
 # returns eigvals and eigvecs of ham (in what basis?)
-E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, nelecs, nroots = nroots); #since spin is none, nelecs=1,1
+E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, nelecs, nroots = nroots);
 if(verbose):
     print("\n1. nelecs = ",nelecs, " nroots = ",nroots); # this matches analytical gd state
     print("FCI energies = ", E_fci);
-    #print(v_fci);
-    
-#### tests
-    
-# repeat w spin pol
-# na, nb = 1,1 gives same as above but 2,0 gives E = 0
-newnelecs = (2,1)
-cisolver = fci.direct_spin1.FCI();
-E_fci, v_fci = cisolver.kernel(h1e, h2e, norbs, newnelecs,nroots=4);
+
+#### spin blind method
+# put all electrons as up and make p,q... spin orbitals
+nelecs = (2,0); # put in all spins as spin up
+norbs = 4;      # now this means spin orbitals
+nroots = 6;
+
+# implement h1e and h2e
+# doing it this way forces floats which is a good fail safe
+h1e_sb = np.zeros((norbs,norbs));
+h2e_sb = np.zeros((norbs,norbs,norbs,norbs));
+
+# on site energy
+h1e_sb[0,0] = epsilon;
+h1e_sb[1,1] = epsilon;
+h1e_sb[2,2] = epsilon;
+h1e_sb[3,3] = epsilon;
+
+# hopping
+h1e_sb[0,2] = t;
+h1e_sb[2,0] = t;
+h1e_sb[1,3] = t;
+h1e_sb[3,1] = t;
+
+# hubbard
+if True:
+    h2e_sb[0,0,1,1] = U+U; # 2nd U from herm conj
+    h2e_sb[2,2,3,3] = U+U;
+else: # this way also works, different shuffling of a's
+    h2e_sb[0,1,1,0] = -U;
+    h2e_sb[1,0,0,1] = -U; # herm conj
+    h2e_sb[2,3,3,2] = -U;
+    h2e_sb[3,2,2,3] = -U; # herm conj
+
+# implement FCISolver object
+cisolver_sb = fci.direct_nosym.FCI();
+cisolver_sb.max_cycle = 100;
+cisolver_sb.conv_tol = 1e-8;
+
+# kernel takes (1e ham, 2e ham, num orbitals, num electrons
+# returns eigvals and eigvecs of ham (in what basis?)
+E_sb, v_sb = cisolver_sb.kernel(h1e_sb, h2e_sb, norbs, nelecs, nroots = nroots);
 if(verbose):
-    print("\n2. nelecs = ",newnelecs);
-    print("FCI energies = ", E_fci);
-    #print(v_fci);
-
-# should work same as first example
-cisolver0 = fci.direct_spin0.FCI()
-e_fci0, v_fci0 = cisolver.kernel(h1e, h2e, norbs, nelecs, nroots = nroots);
-if(verbose):
-    print("\n3. spin0 solver, nelecs = ",nelecs, " nroots = ",nroots);
-    print("FCI energies = ", e_fci0);
-    #print(v_fci0);
-
-  
-  
-#### solve with direct_uhf method
-
-# need to reconfigure h arrays
-
-# h1e = (h1e alpha, h1e beta)
-h1e_alpha = h1e;
-h1e_beta = h1e; # 1e parts do not change, since there is no spin involved
-
-# h2e = h2e alpha-alpha, h2e alpha-beta, h2e beta-beta
-h2e_alpha_alpha = np.zeros((norbs,norbs,norbs,norbs));
-h2e_alpha_beta = np.zeros((norbs,norbs,norbs,norbs));
-h2e_beta_beta = np.zeros((norbs,norbs,norbs,norbs));
-h2e_alpha_beta[0,0,0,0] = U;
-h2e_alpha_beta[1,1,1,1] = U;
-
+    print("\n2. nelecs = ",nelecs, " nroots = ",nroots);
+    print("FCI energies = ", E_sb);
     
-# solve
-cisolver = fci.direct_uhf.FCISolver();
-cisolver.max_cycle = 100
-cisolver.conv_tol = 1e-8
-E_uhf, v_uhf = cisolver.kernel((h1e_alpha, h1e_beta), (h2e_alpha_alpha, h2e_alpha_beta, h2e_beta_beta), norbs, nelecs, nroots=nroots);
-if(verbose):
-    print("\n4. UHF solver"); # this matches analytical gd state
-    print("FCI energies = ", E_uhf);
-    #print(v_fci);
+    
 
