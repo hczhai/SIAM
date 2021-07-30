dab4bf69c456bff4f48740295168f1bcc42f2509
Christian Bunker <christian@bunkerplanet.com>
2021-07-30 12:30:42 -0400

    more dmrg benchmarking (212)

--- run_dmrg_benchmark.py
+++ run_dmrg_benchmark.py
@@ -19,23 +19,23 @@ import matplotlib.pyplot as plt
 #### 3_1_2 system is std benchmark
 
 verbose = 4;
-nleads = (3,2);
-nelecs = (3,3); # half filling
+nleads = (2,2);
+nelecs = (3,2); # half filling
 nelecs_ASU = (sum(nelecs),0); # all spin up formalism
 splots = ['Jtot','occ','delta_occ','Sz','E']; # which subplots to make
 
 #time info
 dt = 0.01;
-tf = 4.0;
+tf = 0.2;
 
 # benchmark with spin free, fci code, std inputs
 fname = td_fci.SpinfreeTest(nleads, nelecs, tf, dt, phys_params = None, verbose = verbose);
-#fname = "dat/SpinfreeTest/312_e6.npy"
-plot.PlotObservables(fname, nleads = nleads, thyb = (1e-8,0.4), splots = splots);
+#fname = "dat/SpinfreeTest/"+str(nleads[0])+"1"+str(nleads[1])+"_e"+str(sum(nelecs))+.npy"
+plot.PlotObservables(fname, nleads = nleads, thyb = (1e-5,0.4), splots = splots);
 
 # test ASU, dmrg code with std inputs
-#datafile = siam_current.DotDataDmrg(nleads,nelecs_ASU,tf,dt,phys_params = None, verbose = verbose);
-datafile = "dat/DotDataDMRG/3_1_2_e6_B0_t0_Vg-0.5.npy"
+datafile = siam_current.DotDataDmrg(nleads,nelecs_ASU,tf,dt,phys_params = None, verbose = verbose);
+#datafile = "dat/DotDataDMRG/"+str(nleads[0])+"_1_"+str(nleads[1])+"_e"+str(sum(nelecs))+"_B0_t0_Vg-0.5.npy"
 plot.PlotObservables(datafile, nleads = nleads, splots = splots);
 
 
